268e12fbebda65eb9f6c1da99c9c03824793dfa2
Johan van Eck <jve.software@gmail.com>
2023-06-01 21:47:18 +0200

    Adding dist subtree

---
+++ dist/js/78.72ca847f.js
@@ -0,0 +1,6 @@
+"use strict";(globalThis["webpackChunkelize_demo"]=globalThis["webpackChunkelize_demo"]||[]).push([[78],{78:(e,t,n)=>{n.r(t),n.d(t,{startStatusTap:()=>i});var o=n(65),s=n(487),r=n(587);
+/*!
+ * (C) Ionic http://ionicframework.com - MIT License
+ */
+const i=()=>{const e=window;e.addEventListener("statusTap",(()=>{(0,o.wj)((()=>{const t=e.innerWidth,n=e.innerHeight,i=document.elementFromPoint(t/2,n/2);if(!i)return;const a=(0,s.a)(i);a&&new Promise((e=>(0,r.c)(a,e))).then((()=>{(0,o.Iu)((async()=>{a.style.setProperty("--overflow","hidden"),await(0,s.s)(a,300),a.style.removeProperty("--overflow")}))}))}))}))}}}]);
+//# sourceMappingURL=78.72ca847f.js.map
