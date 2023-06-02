c0f6eba5002ce8a40af0b181951c35d270149c55
Johan van Eck <jve.software@gmail.com>
2023-06-02 22:15:04 +0200

    Added login functionality & new build

--- js/261.d3ad3f78.js
+++
@@ -1,6 +0,0 @@
-"use strict";(globalThis["webpackChunkelize_demo"]=globalThis["webpackChunkelize_demo"]||[]).push([[261],{261:(t,e,n)=>{n.r(e),n.d(e,{MENU_BACK_BUTTON_PRIORITY:()=>a,OVERLAY_BACK_BUTTON_PRIORITY:()=>o,blockHardwareBackButton:()=>r,startHardwareBackButton:()=>i});
-/*!
- * (C) Ionic http://ionicframework.com - MIT License
- */
-const r=()=>{document.addEventListener("backbutton",(()=>{}))},i=()=>{const t=document;let e=!1;t.addEventListener("backbutton",(()=>{if(e)return;let n=0,r=[];const i=new CustomEvent("ionBackButton",{bubbles:!1,detail:{register(t,e){r.push({priority:t,handler:e,id:n++})}}});t.dispatchEvent(i);const o=async t=>{try{if(null===t||void 0===t?void 0:t.handler){const e=t.handler(a);null!=e&&await e}}catch(e){console.error(e)}},a=()=>{if(r.length>0){let t={priority:Number.MIN_SAFE_INTEGER,handler:()=>{},id:-1};r.forEach((e=>{e.priority>=t.priority&&(t=e)})),e=!0,r=r.filter((e=>e.id!==t.id)),o(t).then((()=>e=!1))}};a()}))},o=100,a=99}}]);
-//# sourceMappingURL=261.d3ad3f78.js.map
--- js/78.5f10fa5e.js
+++ js/78.5f10fa5e.js
@@ -3,4 +3,4 @@
  * (C) Ionic http://ionicframework.com - MIT License
  */
 const i=()=>{const e=window;e.addEventListener("statusTap",(()=>{(0,o.wj)((()=>{const t=e.innerWidth,n=e.innerHeight,i=document.elementFromPoint(t/2,n/2);if(!i)return;const a=(0,s.a)(i);a&&new Promise((e=>(0,r.c)(a,e))).then((()=>{(0,o.Iu)((async()=>{a.style.setProperty("--overflow","hidden"),await(0,s.s)(a,300),a.style.removeProperty("--overflow")}))}))}))}))}}}]);
-//# sourceMappingURL=78.72ca847f.js.map
+//# sourceMappingURL=78.5f10fa5e.js.map
